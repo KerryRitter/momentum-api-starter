953800eb7eb73ecd090a5a7847aa20b7057b3caf
Kerry Ritter <ritter@kerryritter.com>
2021-02-14 09:16:21 -0600

    refactor(): removing starting log message.

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,4 @@ import { platformOak } from "./deps.ts";
 const platform = await platformOak().bootstrapModule(AppModule);
 
 const port = parseInt(Deno.env.get("PORT") ?? "3000");
-console.log(`Starting server at http://localhost:${port}...`);
 await platform.listen({ port });
