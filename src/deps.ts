b80a4d064531a53dfd70449b215398e0ce41ece8
Kerry Ritter <ritter@kerryritter.com>
2021-03-07 16:36:45 -0600

    chore(): upgrade and docs

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,3 @@
-export * from "https://deno.land/x/momentum@v0.7.1/core/mod.ts";
-export * from "https://deno.land/x/momentum@v0.7.1/di/mod.ts";
-export * from "https://deno.land/x/momentum@v0.7.1/platform-oak/mod.ts";
+export * from "https://deno.land/x/momentum@v0.8.1/core/mod.ts";
+export * from "https://deno.land/x/momentum@v0.8.1/di/mod.ts";
+export * from "https://deno.land/x/momentum@v0.8.1/platform-oak/mod.ts";
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
-import { AppModule } from "./app.module.ts";
+import { Application } from "https://deno.land/x/oak/application.ts";
+import { Router } from "https://deno.land/x/oak/router.ts";
 import { platformOak } from "./deps.ts";
+import { AppModule } from "./app.module.ts";
 
-const platform = await platformOak().bootstrapModule(AppModule);
+const app = new Application();
+const router = new Router();
+
+app.addEventListener("listen", ({ hostname, port, secure }) => {
+  console.log([
+    `Listening on `,
+    secure ? "https://" : "http://",
+    hostname ?? "localhost",
+    `:${port}`,
+  ].join(""));
+});
+
+const platform = await platformOak(app, router).bootstrapModule(AppModule);
 
 const port = parseInt(Deno.env.get("PORT") ?? "3000");
 await platform.listen({ port });
